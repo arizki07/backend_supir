01aff28ad472f63aae37512fc4cd4bb570b4992c
arizki07 <ahmadrizki0704@gmail.com>
2024-10-03 19:28:16 +0700

    new file:   app/Http/Controllers/Api/ProfileController.php 	new file:   app/Http/Controllers/Api/UsersController.php 	modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,5 +11,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('test', [AuthController::class, 'index']);
+Route::get('users', [UsersController::class, 'index']);
+Route::get('profile', [ProfileController::class, 'index']);
 Route::post('registered', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'authenticate']);
